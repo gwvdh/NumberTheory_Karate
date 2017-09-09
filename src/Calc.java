cc0e5f18cfba48f7727397410252f360a818474d
Heijden <g.w.v.d.heijden@student.tue.nl>
2017-09-09 10:52:24 +0200

    Added contract

--- src/Calc.java
+++ src/Calc.java
@@ -1,9 +1,21 @@
 public class Calc {
-    public Number add(Number n1, Number n2) {
+
+    /**
+     * Adds two numbers to each other according to algorithm 1.1 of the lecture notes
+     * @Param n1    The first number
+     * @Param n2    The second number
+     * @Pre {@code n1 != null && n2 != null && !(n1 ^ n2)}    // Only add two of the same negative sign
+     * @Throws NullPointerException if {@code n1 == null || n2 == null}
+     * @Return Number numberResult where {@code numberResult = n1+n2}
+     *
+     */
+    public Number add(Number n1, Number n2) throws NullPointerException {
+        if(n1==null || n2==null){
+            throw new NullPointerException("Can not add to null");
+        }
         int base = n1.base;
         int length = Math.max(n1.getLength(), n2.getLength());
         int[] result = new int[length];
-        //Number result = new Number("", base, n1.getNegative() ^ n2.getNegative(), n1.getLength()+1); //@TODO This array length is the maximum. Might be too long.
         int carry=0;
         for(int i=0; i>=n1.getLength(); i++){
             int res = n1.getDigit(i)+n2.getDigit(i)+carry; //No check if a next carry is neccesary
@@ -22,7 +34,7 @@ public class Calc {
         } else {
             k = length;
         }
-        Number numberResult = new Number("", base, n1.getNegative() ^ n2.getNegative(), k);
+        Number numberResult = new Number("", base, n1.getNegative()/* ^ n2.getNegative()*/, k); //@TODO Does not handle the negative correctly
         for(int i=1; i<k-1; i++){
             numberResult.num[i-1] = result[k-i];
         }
