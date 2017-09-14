3233b2506baeae35ae14e8fbfaf0a0d424b4f236
Addono <a.d.knapen@protonmail.com>
2017-09-14 12:25:04 +0200

    new: added contracts to the number class

--- src/Number.java
+++ src/Number.java
@@ -1,23 +1,65 @@
+/**
+ * Represents large signed integer numbers.
+ */
 public class Number implements Comparable<Number> {
     int base;
     boolean negative;
     int[] num;
 
+    /**
+     * Creates a new {@code Number} object.
+     * @pre {@code {\forall i; value.has(i); value[i] >= 0} && value != null && base > 0}
+     * @param value The number represented as an array of integers, with index 0 representing the least important digit
+     *              and becoming increasingly important with increasing index. Every number in this array should be >0.
+     * @param base The base (radix) of the number, should be positive and non zero.
+     * @param negative True if the number should be negative, false otherwise.
+     */
     public Number(int[] value, int base, boolean negative) {
+        // Check if value is not null
+        if (value == null) {
+            throw new IllegalArgumentException("Value may not be null");
+        }
+
+        // Check if all numbers in value are at least positive.
+        for(int i = value.length - 1; i >= 0; i--) {
+            if (value[i] < 0) {
+                throw new IllegalArgumentException("The value of the integer on index " + i +
+                        " should be larger or equal to 0, is " + value[i]);
+            }
+        }
+
+        // Check if the base is positive.
+        if (base <= 0) {
+            throw new IllegalArgumentException("Base should be positive and non zero, is " + base);
+        }
+
         this.base = base;
         this.negative = negative;
         this.num = value.clone();
     }
 
+    /**
+     * Gives the amount of digits the number consists of, including leading zeros.
+     * @return The amount of digits the number consists of, including leading zeros.
+     */
     public int getLength() {
         return num.length;
-
     }
 
+    /**
+     * Gives if the number is positive or not.
+     * @return True if the number is negative, false if not.
+     */
     public boolean getNegative() {
         return negative;
     }
 
+    /**
+     * Gives access to one of the digits of the number.
+     * @param index The index of the number to be retrieved. {@code 0} indicates the least important digit, {@code getLength()} the
+     *              most important digit.
+     * @return The digit present at the digit indicated by {@code index}.
+     */
     public int getDigit(int index) {
         return num[index];
     }
