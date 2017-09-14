68381aebf08f84fc1cc06f5c83843e00a2beeecb
Addono <a.d.knapen@protonmail.com>
2017-09-14 11:32:12 +0200

    new: added test cases for Number.getNegative()

--- src/NumberTest.java
+++ src/NumberTest.java
@@ -16,6 +16,21 @@ class NumberTest {
 
     @org.junit.jupiter.api.Test
     void getNegative() {
+        testNegative(new Number(new int[] {0}, 5, false), false);
+        testNegative(new Number(new int[] {0,1}, 5, false), false);
+        testNegative(new Number(new int[] {0,1,2}, 5, false), false);
+        testNegative(new Number(new int[] {0,1,2,3}, 5, false), false);
+        testNegative(new Number(new int[] {1,2,3,0}, 5, false), false);
+
+        testNegative(new Number(new int[] {0}, 5, true), true);
+        testNegative(new Number(new int[] {0,1}, 5, true), true);
+        testNegative(new Number(new int[] {0,1,2}, 5, true), true);
+        testNegative(new Number(new int[] {0,1,2,3}, 5, true), true);
+        testNegative(new Number(new int[] {1,2,3,0}, 5, true), true);
+    }
+
+    void testNegative(Number n, boolean expected) {
+        assertEquals(n.getNegative(), expected);
     }
 
     @org.junit.jupiter.api.Test
