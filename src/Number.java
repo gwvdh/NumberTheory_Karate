13d7f876c4c042b4b7facd4cba712d329e276493
Addono <a.d.knapen@protonmail.com>
2017-09-14 12:09:36 +0200

    new: added test cases for Number.compareTo and Number.getDigit

--- src/Number.java
+++ src/Number.java
@@ -28,27 +28,33 @@ public class Number implements Comparable<Number> {
         int otherLength = o.getLength();
         int thisLength = getLength();
 
+        boolean thisNegative = getNegative();
+        boolean otherNegative = o.getNegative();
+
         int maxLength = Math.max(otherLength, thisLength);
 
+        // Loop through all digits from the most import digit to the least important digit.
         for (int i = maxLength - 1; i >= 0; i--) {
             int difference = 0;
 
-            // Within the length of a normal digit.
-            if (i < otherLength) {
-                if (o.getNegative()) {
-                    difference -= o.getDigit(i);
-                } else {
-                    difference += o.getDigit(i);
-                }
-            }
-
+            // Check if the index is within the length of this number.
             if (i < thisLength) {
-                if (getNegative()) {
+                if (thisNegative) {
                     difference -= getDigit(i);
                 } else {
                     difference += getDigit(i);
                 }
             }
+
+            // Check if the index is within the length of the other number.
+            if (i < otherLength) {
+                if (otherNegative) {
+                    difference += o.getDigit(i);
+                } else {
+                    difference -= o.getDigit(i);
+                }
+            }
+
 
             if (difference != 0) {
                 return difference;
--- src/NumberTest.java
+++ src/NumberTest.java
@@ -35,10 +35,67 @@ class NumberTest {
 
     @org.junit.jupiter.api.Test
     void getDigit() {
+        testDigit(new Number(new int[] {0}, 5, false), 0, 0);
+        testDigit(new Number(new int[] {0,1}, 5, true), 1, 1);
+        testDigit(new Number(new int[] {0,1,2}, 5, false), 2, 2);
+        testDigit(new Number(new int[] {0,1,2,3}, 5, true), 3, 3);
+        testDigit(new Number(new int[] {1,2,3,0}, 5, false), 3, 0);
+    }
+
+    void testDigit(Number n, int index, int expected) {
+        assertEquals(n.getDigit(index), expected);
     }
 
     @org.junit.jupiter.api.Test
     void compareTo() {
+        Number p0 = new Number(new int[] {0}, 5, false);
+        Number p1 = new Number(new int[] {1}, 5, false);
+        Number p04 = new Number(new int[] {0,4}, 5, false);
+        Number p0512494 = new Number(new int[] {0,5,1,2,4,9,4}, 5, false);
+
+        Number n0 = new Number(new int[] {0}, 5, true);
+        Number n1 = new Number(new int[] {1}, 5, true);
+        Number n04 = new Number(new int[] {0,4}, 5, true);
+        Number n0512494 = new Number(new int[] {0,5,1,2,4,9,4}, 5, true);
+
+        // Test equivalence of both positive and negative version of zero.
+        compareTest(p0, p0, 0);
+        compareTest(p0, n0, 0);
+        compareTest(n0, p0, 0);
+        compareTest(n0, n0, 0);
+
+        // Test equivalence of two large numbers.
+        compareTest(p0512494, p0512494, 0);
+        compareTest(n0512494, n0512494, 0);
+
+        // Test comparison of positive numbers
+
+
+        // Test comparison of negative numbers
+
+        // Test comparison of mixed positive and negative numbers
+        compareTest(p0512494, n0512494, 1);
+        compareTest(n0512494, p0512494, -1);
+        compareTest(p0512494, p0, 1);
+        compareTest(p0512494, n0, 1);
+        compareTest(n0512494, p0, -1);
+        compareTest(n0512494, n0, -1);
+        compareTest(p0512494, n04, 1);
+        compareTest(p0512494, p04, 1);
+        compareTest(n0512494, n04, -1);
+        compareTest(n0512494, p04, -1);
+    }
+
+    void compareTest(Number n1, Number n2, int expected) {
+        int actual = n1.compareTo(n2);
+
+        if (expected > 0) {
+            assertTrue(actual > 0, "Compare test expected a positive number, got " + actual);
+        } else if (expected < 0) {
+            assertTrue(actual < 0, "Compare test expected a negative number, got " + actual);
+        } else {
+            assertTrue(actual == 0, "Compare test expected 0, got " + actual);
+        }
     }
 
 }
